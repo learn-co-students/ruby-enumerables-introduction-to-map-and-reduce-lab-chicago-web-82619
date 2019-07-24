c1d8091903dcb9410e51777129dceee87dcf64d8
Justice Marks <her@shedaddy.com>
2019-07-24 20:07:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # My Code here....
+
+def map_to_negativize(source_array) 
+  result = []
+  
+  for i in source_array {
+    result.push(source_array[i]*-1)
+    
+  }
+  
+  return result
+end
+
+=begin
+def map_to_no_change(source_array) do 
+end
+
+def map_to_double(source_array) do 
+end
+
+def map_to_square(source_array) do
+end
+=end
