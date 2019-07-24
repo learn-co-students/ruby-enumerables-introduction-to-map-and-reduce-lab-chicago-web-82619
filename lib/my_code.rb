085e1488c6a16900904d14a979d26078e9022c46
Justice Marks <her@shedaddy.com>
2019-07-24 20:25:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,22 +2,85 @@
 
 def map_to_negativize(source_array) 
   result = []
+   i=0
+   while i<source_array.length do 
+     result.push(source_array[i]*-1)
+     i+= 1
+   end
+   
+  return result
+end
+
+
+def map_to_no_change(source_array)  
+  result = []
+   i=0
+   while i<source_array.length do 
+     result.push(source_array[i])
+     i+= 1
+   end
+   
+  return result
+end
+
+
+def map_to_double(source_array)  
+  result = []
+   i=0
+   while i<source_array.length do 
+     result.push(source_array[i]*2)
+     i+= 1
+   end
+   
+  return result
+end
+
+def map_to_square(source_array) 
+  result = []
+   i=0
+   while i<source_array.length do 
+     result.push(source_array[i]**2)
+     i+= 1
+   end
+   
+  return result
+end
+
+def reduce_to_total(source_array, starting_point = 0)
   
-  for i in source_array {
-    result.push(source_array[i]*-1)
-    
-  }
+ 
+  result = starting_point 
+  
+  
+
+  i=0
+  while i<source_array.length do
+    result+= source_array[i]
+    i+=1
+  end
   
   return result
 end
 
-=begin
-def map_to_no_change(source_array) do 
+def reduce_to_all_true(source_array)
+  result = TRUE
+  i=0
+  while i<source_array.length do
+    result = result && !!source_array[i]
+    i+=1
+  end
+  
+  return result
 end
 
-def map_to_double(source_array) do 
+def reduce_to_any_true(source_array)
+  result = FALSE
+  i=0
+  while i<source_array.length do
+    result = result || !!source_array[i]
+    i+=1
+  end
+  
+  return result
 end
 
-def map_to_square(source_array) do
-end
-=end
