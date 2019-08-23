040389f3661f887ac8a01c2e7a229873d91aa1e0
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-23 03:07:03 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,58 @@
 # My Code here....
+def map_to_negativize(source_array)
+  result = []
+  source_array.length.times { |i|
+    result[i] = source_array[i] * -1
+  }
+  return result
+end
+
+def map_to_no_change(source_array)
+  result = []
+  source_array.length.times { |i|
+    result[i] = source_array[i]
+  }
+  return result
+end
+
+def map_to_double(source_array)
+  result = []
+  source_array.length.times { |i|
+    result[i] = source_array[i] * 2
+  }
+  return result
+end
+
+def map_to_square(source_array)
+  result = []
+  source_array.length.times { |i|
+    result[i] = source_array[i] ** 2
+  }
+  return result
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  result = starting_point 
+  source_array.length.times { |i|
+    result += source_array[i]
+  }
+  return result
+end
+
+def reduce_to_all_true(source_array)
+  source_array.length.times { |i|
+    if !source_array[i] 
+      return false 
+    end
+  }
+  return true
+end
+
+def reduce_to_any_true(source_array)
+  source_array.length.times { |i|
+    if source_array[i] 
+      return true 
+    end
+  }
+  return false
+end
